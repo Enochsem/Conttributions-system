13c45313c7d25b6e326e15b8979f2dfc4946949b
Enochsem <enochsem@gmail.com>
2021-07-03 20:38:44 -0700

    file title with timestamp & contributor sms added

--- methods.py
+++ methods.py
@@ -21,17 +21,34 @@ def beneficiary_sms(contributor_name, contributor_amount, contributor_contact, b
     total_amount = get_total(beneficiary_name)
 
     recipient = beneficiary_number
-    sender = "hello".upper()  #Rev Nickel
+    sender = "Nickel".upper()  #Rev Nickel
     message = """
-    You have received {:,.2f}GHS 
-    from {} ({}). 
-    Your total balance is {:,.2f}GHS. 
+    You have received {:,.2f}GHS from {} ({}). Your total balance is {:,.2f}GHS. 
     Thank you
     """.format(float(contributor_amount),contributor_name,contributor_contact,float(total_amount))
     
     sms = SMS(recipient, sender, message)
     sms.send()
 
+    contributor_sms(contributor_name, contributor_amount, contributor_contact)
+
     print("sent")
 
+
+
+
+def contributor_sms(contributor_name, contributor_amount, contributor_contact):
+
+    recipient = contributor_contact
+    sender = "Nickel".upper()  #Rev Nickel
+    message = """
+    Dear {}, your donation of {:,.2f} GHS have been recieved with thanks. We are grateful for your support. God bless you
+
+    www.evelynnickel.com
+    """.format(contributor_name,float(contributor_amount))
+    
+    sms = SMS(recipient, sender, message)
+    sms.send()
+
+    print("sent contributor_sms")
 
